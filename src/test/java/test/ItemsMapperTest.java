8e53ec600ea19a03e14c4449dd42c55356c13065
xiezhjinbu <xiezhjinbu@hotmail.com>
2017-05-18 21:29:14 +0800

    读取企业信息正常
    读取企业信息正常

--- src/test/java/test/ItemsMapperTest.java
+++ src/test/java/test/ItemsMapperTest.java
@@ -2,6 +2,7 @@ package test;
 
 import common.ContactInformation;
 import common.GlobalData;
+import dao.CompanyInfoMapper;
 import dao.ItemsMapper;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
@@ -15,9 +16,10 @@ public class ItemsMapperTest {
     public void selectByPrimaryKey() throws Exception {
         ApplicationContext ac = new ClassPathXmlApplicationContext(new String("applicationContext.xml"));
         //System.out.println(ac.toString());
-        ItemsMapper itemsMapper=ac.getBean("itemsMapper",ItemsMapper.class);
-        System.out.println(itemsMapper.toString());
-
+        //ItemsMapper itemsMapper=ac.getBean("itemsMapper",ItemsMapper.class);
+        //System.out.println(itemsMapper.toString());
+        CompanyInfoMapper companyInfoMapper=ac.getBean("companyInfoMapper",CompanyInfoMapper.class);
+        System.out.println(companyInfoMapper.selectCompanyInfo());
 
     }
 
